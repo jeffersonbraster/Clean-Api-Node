61277543d0d98204a1702c93a13e3e0546abfd09
Jefferson <jeffersonbraster@gmail.com>
2020-10-28 19:10:06 -0300

    retirando codigo duplicado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,10 +17,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passworcConfirmation } = httpRequest.body
+      if (password !== passworcConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
