1542e0d86d921adf5cfbe7d0c2a3c4540e138551
Jefferson <jeffersonbraster@gmail.com>
2020-10-29 16:30:36 -0300

    validar email correto

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -2,10 +2,10 @@ import { EmailValidatorAdapter } from './email-validator'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
-  isEmail(): boolean {
+  isEmail (): boolean {
     return true
-  })
-})
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
   test('should return if validator returns false', () => {
@@ -19,5 +19,12 @@ describe('EmailValidator Adapter', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valido_email@gmail.com')
     expect(isValid).toBe(true)
+  })
+
+  test('should call validator with correcy email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpay = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@gmail.com')
+    expect(isEmailSpay).toHaveBeenCalledWith('any_email@gmail.com')
   })
 })
