b61dc312a41ad0618e61a1d08c23e3575d3db95e
Jefferson <jeffersonbraster@gmail.com>
2020-10-29 16:17:52 -0300

    criando mock do validator no test

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  })
+})
 
 describe('EmailValidator Adapter', () => {
   test('should return if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalido_email@gmail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('should return if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valido_email@gmail.com')
+    expect(isValid).toBe(true)
   })
 })
